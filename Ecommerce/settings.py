17a1fafa8b7d0564f548f28e5f856ec2233a9f7d
Edyl07 <demba94diagana@gmail.com>
2020-09-19 17:24:23 +0000

    models & migration

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'py_ecommerce',
+        'USER': 'postgres',
+        'PASSWORD': 'demba94',
+        'HOST': '',
+        'PORT': '5432',
     }
 }
 
