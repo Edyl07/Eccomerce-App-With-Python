7a88c69ec1d69ad81f6ccac621c8b70c6a7cf1cd
Edyl07 <demba94diagana@gmail.com>
2020-09-20 21:54:37 +0000

    add/update cart event handlers

---
+++ static/js/cart.js
@@ -0,0 +1,16 @@
+let updateBtns = document.getElementsByClassName('update-cart');
+
+for (let i = 0; i < updateBtns.length; i++){
+    updateBtns[i].addEventListener('click', function () {
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log('Product ID: ', productId, 'Action: ', action)
+
+        console.log('User: ', user);
+        if (user === 'AnonymousUser'){
+            console.log('Not logged in')
+        }else{
+            console.log('User is logged in, sending data...')
+        }
+    })
+}
