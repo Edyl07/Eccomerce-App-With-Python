b1ed81ed76312fbecc291977394121596954f06b
Edyl07 <demba94diagana@gmail.com>
2020-10-05 16:09:12 +0000

    csrf_token

--- store/views.py
+++ store/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import json
 from .models import *
+import datetime
 
 
 # Create your views here.
@@ -42,6 +43,9 @@ def cart(request):
     return render(request, 'store/cart.html', context)
 
 
+# from django.views.decorators.csrf import csrf_exempt
+# @csrf_exempt
+
 def checkout(request):
     if request.user.is_authenticated:
         customer = request.user.customer
@@ -80,3 +84,29 @@ def updateItem(request):
     if orderItem.quantity <= 0:
         orderItem.delete()
     return JsonResponse('Item was addedd', safe=False)
+
+
+def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+        if total == order.get_cart_total :
+            order.complete == True
+            order.save()
+        if order.shipping == True :
+            ShippingAddress.objects.create(
+                customer=customer,
+                order=order,
+                address=data['shipping']['address'],
+                city=data['shipping']['city'],
+                state=data['shipping']['state'],
+                zipcode=data['shipping']['zipcode']
+            )
+    else:
+        print('User is not logged in..')
+    return JsonResponse('Payment Complete!', safe=False)
